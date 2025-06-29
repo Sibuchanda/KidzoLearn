bc5fb9b70007346c698cb01ec9d9d62d5937f4a7
Sibuchanda <sibuchanda457@gmail.com>
2025-06-29 08:18:10 +0530

    Ready for production environment

--- backend/index.js
+++ backend/index.js
@@ -4,15 +4,12 @@ import cors from 'cors'
 import cookieParser from "cookie-parser";
 import userRoute from './routes/user.js';
 import taskRoute from './routes/task.js';
-
 import connectDB from "./model/connect.js";
 
 const app = express();
 
 dotenv.config();
 const PORT = process.env.PORT || 8000;
-// const DB_URI="mongodb+srv://sibuchanda457:fanCgDryz1wEgaqh@cluster0.nq9wq9f.mongodb.net/kidzo_school"
-const DB_URI="mongodb://127.0.0.1:27017/kidzo_school";
 
 app.use(express.json());
 app.use(cookieParser());
@@ -22,9 +19,10 @@ app.use(cors({
     methods:"GET,POST,PUT,DELETE",
     allowedHeaders: ["Content-Type", "Authorization"]
 }))
-//--------- Database connection ---------
-connectDB(DB_URI);
+//Databae connecion
+connectDB(process.env.DB_URL);
 
+//Routes
 app.use("/user",userRoute);
 app.use("/task",taskRoute);
 
